8c0362a782e1f3fa6fbe08f61a8b084f3a819c1b
thecountrymouth11 <thecountrymouth@gmail.com>
2023-03-30 20:38:00 +0530

    dark-mode-using-state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TEXT-UTILS" aboutText="About TEXT-UTILS" />
       <div className="container">
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
