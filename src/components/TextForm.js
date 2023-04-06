e338ea24c75b558c65177391f2a1f2deb2ec8992
thecountrymouth11 <thecountrymouth@gmail.com>
2023-04-06 21:32:17 +0530

    minutes-fix

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -85,7 +85,7 @@ export default function TextForm(props) {
         <p>
           {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters
         </p>
-        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
         <h3>Preview</h3>
         <p>{text.length > 0 ? text : "enter something above to preview..."}</p>
       </div>
